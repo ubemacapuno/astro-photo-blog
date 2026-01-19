2290bc61fb49778115d544041c6b437d36811734
Corey Developer <corey.damocles@fastdms.com>
2026-01-19 12:39:22 -0700

    feat: Update UI components for improved accessibility and styling
    - Enhanced Button component with text color for better visibility.
    - Adjusted Footer component layout for consistent spacing.
    - Updated Greeting component button styles for a modern look.
    - Refactored Menu component for improved accessibility and visual feedback.
    - Modified BaseLayout to ensure proper content structure and responsiveness.
    - Improved global styles for dark mode and added link styles in main content.

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -4,16 +4,12 @@ const mainMenu = document.getElementById("main-menu");
 menuButton?.addEventListener("click", () => {
   const isExpanded = menuButton.getAttribute("aria-expanded") === "true";
   
-  // Toggle aria-expanded
   menuButton.setAttribute("aria-expanded", `${!isExpanded}`);
   
-  // Toggle visibility using Tailwind classes
   if (isExpanded) {
-    // Hide menu
     mainMenu?.classList.add("hidden");
     mainMenu?.classList.remove("block");
   } else {
-    // Show menu
     mainMenu?.classList.remove("hidden");
     mainMenu?.classList.add("block");
   }
